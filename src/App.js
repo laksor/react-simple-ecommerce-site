764b91b897fce12121fc7b496268aa703a9c7684
laskor <al.moksud@gmail.com>
2022-03-25 13:54:55 -0400

    header done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header/Header';
+import Shop from './Components/Shop/Shop';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header></Header>
+      <Shop></Shop>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Header.css';
+import logo from '../../images/Logo.svg';
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <nav className='header'>
+            <img src={logo} alt='logo'></img>
+            <div>
+                <a href="/shop">Shop</a>
+                <a href="/orders">Orders</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/about">About</a>
+            </div>
+        </nav>
     );
 };
 
