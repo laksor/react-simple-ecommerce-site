4833a2543106fe885cb0134f6b38e947908531ab
laskor <al.moksud@gmail.com>
2022-03-27 21:04:13 -0400

    header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,7 +8,6 @@ const Header = () => {
             <div>
                 <a href="/shop">Shop</a>
                 <a href="/orders">Orders</a>
-                <a href="/inventory">Inventory</a>
                 <a href="/about">About</a>
             </div>
         </nav>
