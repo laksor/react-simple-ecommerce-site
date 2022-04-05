fe6b7d1cd1c3f3c3be2eb02503e026baaf8daa17
laskor <al.moksud@gmail.com>
2022-04-04 23:24:43 -0400

    routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header/Header';
+import Inventory from './Components/Inventory/Inventory';
+import Orders from './Components/Orders/Orders';
 import Shop from './Components/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Routes>
+      <Route path='/' element={<Shop></Shop>}></Route>
+      <Route path='/shop' element={<Shop></Shop>}></Route>
+      <Route path='/orders' element={<Orders></Orders>}></Route>
+      <Route path='/inventory' element={<Inventory></Inventory>}></Route>
+      </Routes>
+      
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,6 +8,7 @@ const Header = () => {
             <div>
                 <a href="/shop">Shop</a>
                 <a href="/orders">Orders</a>
+                <a href="/inventory">Inventory</a>
                 <a href="/about">About</a>
             </div>
         </nav>
